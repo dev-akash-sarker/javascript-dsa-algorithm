a199249a8c22ccf63f1ff1449114dc8edbfb4bb9
Akash <45712184+dev-akash-sarker@users.noreply.github.com>
2025-12-06 02:21:48 +0600

    al

--- main.js
+++ main.js
@@ -1,32 +1,20 @@
-// let data = [30,20,45,76,20,80]
+let data = [20,40,60,5,10,70,80,99];
+console.log(data)
+function searchEL(){
 
-// let position = document.getElementById('position').value;
+let item = document.getElementById('search').value;
+item = parseInt(item);
+let index = undefined;
 
-// // for (let i = 3 ; i < data.length - 1;i++){
-// //     console.log(data[i])
-// //     data[i] = data[i+1]
-// // }
-
-// // console.log(data)
-
-// for (let i = position; i < data.length - 1 ; i++){
-//     console.log(data[i])
-//     data[i] = data[i + 1]
-// }
-
-// data.length = data.length - 1
-
-// console.log(data)
-
-function deleteEl (){
-    let data = [30,20,45,76,20,80]
-    let position = document.getElementById('position').value;
-    position = parseInt(position)
-
-    for (let i = position; i < data.length - 1; i++){
-        data[i] = data[i+1]
+for(let i = 0; i < data.length; i++){
+    // console.log(data[i])
+    if(data[i] === item){
+        index = i;
+        break;
     }
-    data.length = data.length - 1
-    console.log(data)
 }
 
+alert(index)
+
+console.log(index)
+}
