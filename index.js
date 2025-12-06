6b59aff378d65b47bdc898bf061b111d751ecc7f
Akash <45712184+dev-akash-sarker@users.noreply.github.com>
2025-12-07 01:08:03 +0600

    all

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // travarse
 
-var yelo = [9,7,3,2,1]
+var yelo = [9, 7, 3, 2, 1];
 
 // here yelo is a array but what this variable have?
 // so the way to read is travarse. what yelo have inside.
 
 // insertion
 
-var yelo = [9,7,3,2,1,10,12]
+var yelo = [9, 7, 3, 2, 1, 10, 12];
 
 // here 10 and 10 are added this is insertion
 
 // deletion
 
-var yelo = [9,7,3]
+var yelo = [9, 7, 3];
 // here we delete or remove 2,1,10,12 this is deletion
 
 // searching
@@ -22,7 +22,7 @@ var yelo = [9,7,3]
 
 // sorting
 
-const abc = [8,2,1,6,0,4,3,7,5] 
+const abc = [8, 2, 1, 6, 0, 4, 3, 7, 5];
 
 // this is unorganize array and when we do sorting like
 // lower to higher or higher to lower
@@ -31,10 +31,10 @@ const abc = [8,2,1,6,0,4,3,7,5]
 
 // merging
 
-const a = [1,2];
-const b = [3,4];
+const a = [1, 2];
+const b = [3, 4];
 
-const c = [1,2,3,4]
+const c = [1, 2, 3, 4];
 
 // this is like a merging
 
