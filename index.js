7077d41243264829438698361112f249a18f865e
Akash <45712184+dev-akash-sarker@users.noreply.github.com>
2025-12-10 20:50:34 +0600

    hello

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
-// travarse
+let A = [1,2,3];
+let B = [4,5,6,11];
+let C = [];
 
-var yelo = [9, 7, 3, 2, 1];
+for(let i = 0; i < A.length ; i++){
+    C[i]= A[i]
+}
+for(let i = 0; i < B.length; i++){
+    C[A.length + i]= B[i]
+}
 
-// here yelo is a array but what this variable have?
-// so the way to read is travarse. what yelo have inside.
 
-// insertion
-
-var yelo = [9, 7, 3, 2, 1, 10, 12];
-
-// here 10 and 10 are added this is insertion
-
-// deletion
-
-var yelo = [9, 7, 3];
-// here we delete or remove 2,1,10,12 this is deletion
-
-// searching
-
-// in yelo if i need a spechific data like 3 this is searching.
-
-// sorting
-
-const abc = [8, 2, 1, 6, 0, 4, 3, 7, 5];
-
-// this is unorganize array and when we do sorting like
-// lower to higher or higher to lower
-// 0,1,2,3 lower to hight
-// 3,2,1,0 higher to lower
-
-// merging
-
-const a = [1, 2];
-const b = [3, 4];
-
-const c = [1, 2, 3, 4];
-
-// this is like a merging
-
-// Difference between linear and non linear
+console.log(C)
